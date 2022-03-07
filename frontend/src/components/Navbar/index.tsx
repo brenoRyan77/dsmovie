8e67f12ee8e3cb8ad2312704cedb40c388fcdb97
Ryan De Oliveira Lima <bryan@indracompany.com>
2022-03-07 11:58:06 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import {ReactComponent as GithunIcon} from 'assents/img/github.svg';
+import './style.css';
+function Navbar(  ){
+    
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/brenoRyan77">
+                        <div className="dsmovie-contact-container">
+                            <GithunIcon />
+                            <p className="dsmovie-contact-link">/brenoRyan77</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+export default Navbar;
