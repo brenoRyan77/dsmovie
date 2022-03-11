cbfc1ed5adf8dfbbfdd6a9402b4d1de81c294e34
Ryan De Oliveira Lima <bryan@indracompany.com>
2022-03-11 09:21:04 -0300

    feat: first request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,18 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "Utils/requests";
 
 function Listing() {
+
+    //Forma errada
+
+    axios.get(`${BASE_URL}/movies?size=10&page=0`)
+    .then(response => {
+        console.log(response.data);
+    })
+
+
 
     return (
         <>
