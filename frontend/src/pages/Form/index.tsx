1b9d0657147c32065562437d8149c146caef43ec
Ryan De Oliveira Lima <bryan@indracompany.com>
2022-03-11 12:00:30 -0300

    fix: remoção de linhas

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -2,9 +2,6 @@ import FormCard from 'components/FormCard';
 import { useParams } from 'react-router-dom';
 import { Movie } from 'types/movie';
 
-
-
-
 function Form() {
     const params = useParams();
 
